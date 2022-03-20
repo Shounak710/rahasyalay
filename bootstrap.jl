8b77c514487bc745148d35d6a89533895120cad3
shounak710 <shounak647@gmail.com>
2022-03-20 17:32:16 +0900

    Deleting all files

--- bootstrap.jl
+++
@@ -1,5 +0,0 @@
-(pwd() != @__DIR__) && cd(@__DIR__) # allow starting app from bin/ dir
-
-using RahasyalayBackend
-push!(Base.modules_warned_for, Base.PkgId(RahasyalayBackend))
-RahasyalayBackend.main()
--- test/runtests.jl
+++
@@ -1,3 +0,0 @@
-using RahasyalayBackend, Test
-# implement your tests here
-@test 1 == 1
