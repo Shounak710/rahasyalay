42dbc83e0efb92a6b1283afd33c66dc3daa28610
shounak710 <shounak647@gmail.com>
2022-03-21 01:38:32 +0900

    Resolving secret key issue

--- rahasyalay/settings.py
+++ rahasyalay/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kv4r@5x-jng40s&8^v32$p1q_6h$^h^7nl!ywc5=8do_@_-g^o'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
